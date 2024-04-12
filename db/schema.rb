e53e5db3624c2f667ab1ecd331ab6d03b704bb3f
malarkee <ethanl@soup>
2024-04-12 17:19:48 -0400

    Fixed dropdown, added links to navbar, centered page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_195026) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_203230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_195026) do
     t.string "age_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views"
   end
 
 end
